61855825b9a08f530d2adb4b6c93572b965cd788
Sean Schmidt <schmidtsean048@gmail.com>
2021-05-24 16:00:41 -0600

    board created

---
+++ src/components/Game.js
@@ -0,0 +1,27 @@
+import '../styles/Styles.css';
+
+const Game = () => {
+  return (
+    <div className='game'>
+      <div className='row'>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+      </div>
+      <div className='row'>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+      </div>
+      <div className='row'>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+      </div>
+
+    </div>
+    
+  )
+}
+
+export default Game;
