c30378e5a70cf25360fe30b2b9b2f9960da69172
Sean Schmidt <schmidtsean048@gmail.com>
2021-05-31 12:05:20 -0600

    O, X

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
-const Box = () => {
+const Box = (props) => {
   const [letter, setLetter] = useState('')
 
   function toggleLetter() {
     if (letter === '') {
-      setLetter ('X')
+      setLetter (props.currentState)
+      props.changeTurn()
     }
   }
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,23 +1,32 @@
+import { useState } from 'react';
 import '../styles/Styles.css';
 import Box from './Box';
 
 const Game = () => {
+
+  const [turn, setTurn] = useState('X')
+
+    function changeTurn() {
+      setTurn (turn => turn === 'X' ? 'O' : 'X')
+    }
+
+
   return (
     <div className='game'>
       <div className='row'>
-        <Box id={0} />
-        <Box id={1} />
-        <Box id={2} />
+        <Box id={0} currentState={turn} changeTurn={changeTurn}/>
+        <Box id={1} currentState={turn} changeTurn={changeTurn}/>
+        <Box id={2} currentState={turn} changeTurn={changeTurn}/>
       </div>
       <div className='row'>
-        <Box id={0} />
-        <Box id={1} />
-        <Box id={2} />
+        <Box id={0} currentState={turn} changeTurn={changeTurn}/>
+        <Box id={1} currentState={turn} changeTurn={changeTurn}/>
+        <Box id={2} currentState={turn} changeTurn={changeTurn}/>
       </div>
       <div className='row'>
-        <Box id={0} />
-        <Box id={1} />
-        <Box id={2} />
+        <Box id={0} currentState={turn} changeTurn={changeTurn}/>
+        <Box id={1} currentState={turn} changeTurn={changeTurn}/>
+        <Box id={2} currentState={turn} changeTurn={changeTurn}/>
       </div>
 
     </div>
